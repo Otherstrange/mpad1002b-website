fa29a8e0bc4177471b4d31ae56eb8ed4efbf2f20
Henri Linkletter <henrilinkletter@gmail.com>
2024-04-20 18:07:01 -0400

    scripting and link changes for live preview
    Co-authored-by: Ethan Benjamin <ethan-pdf@users.noreply.github.com>

--- movie.js
+++ movie.js
@@ -5,7 +5,13 @@ let optionsButtons = document.getElementById("optionsButtonsListId"); // id for 
 let iframePlayer = document.getElementById("vimeoId"); // class for the iframe that contains the video player
 // const player = new Vimeo.Player(iframePlayer); // Creates a new Vimeo player
 let menu_open = 0
+let buttons_show = false
 
+let currentsrc = "";
+
+if (localStorage.getItem(currentsrc) === null) {
+    localStorage.setItem(currentsrc, 'https://player.vimeo.com/video/935318444?h=1c05a2036a&autoplay=1&title=0&byline=0&portrait=0');
+}
 
 menuButton.addEventListener("click", handleMenu);
 videobutton.addEventListener("click", handleMenu);
@@ -34,34 +40,31 @@ function handleClickOutside(event) { // Handles the ability to close the sub men
     }
 }
 
+iframePlayer.addEventListener("play", onVimeoPlayerReady); // Calls the function onVimeoPlayerReady when the iframe loads
+
 function onVimeoPlayerReady() { // Handles the ability for buttons to appear when the Vimeo video ends
     const player = new Vimeo.Player(iframePlayer); // Creates a new Vimeo player
 
+    iframePlayer.src = localStorage.getItem(currentsrc);
+
     player.on('ended', function() {
-        optionsButtons.style.display="flex";
 
         let srcValue = iframePlayer.src;
 
-        for (let i = 0; i < videosArray.length; i++) {
-            if (srcValue === videosArray[i].src) {
-                videosArray[i].watched = true;
-            }
-        }
+        localStorage.setItem(srcValue, true);
+
+        console.log(localStorage.getItem(srcValue));
 
         updateButtons();
         
     });
-
-    player.on('play', function() {
-        optionsButtons.style.display="none";
-        console.log('play')
-    });
 }
-
 
 function updateButtons() { // Handles the ability to update the buttons on the video player
     let option1 = document.getElementById("option1Class");
     let option2 = document.getElementById("option2Class");
+
+    optionsButtons.style.display = "flex";
 
     let srcValue = iframePlayer.src;
     
@@ -71,20 +74,19 @@ function updateButtons() { // Handles the ability to update the buttons on the v
             option2.innerHTML = videosArray[i].option2;
             option1.addEventListener("click", function() {
                 iframePlayer.src = videosArray[i].option1Link;
-                updateButtons();
+                localStorage.setItem(currentsrc, videosArray[i].option1Link);
+                optionsButtons.style.display = "none";
                 onVimeoPlayerReady();
             });
             option2.addEventListener("click", function() {
                 iframePlayer.src = videosArray[i].option2Link;
-                updateButtons();
+                localStorage.setItem(currentsrc, videosArray[i].option2Link);
+                optionsButtons.style.display = "none";
                 onVimeoPlayerReady();
             });
-            console.log(videosArray[i].desc)
-            console.log(videosArray[i].watched)
         }
     }
 }
-
 
 let videosArray = [
     { 
@@ -93,7 +95,6 @@ let videosArray = [
         option2: 'Let Henri stay',
         option1Link:"https://player.vimeo.com/video/935984923?h=47d7c5698b&autoplay=1&title=0&byline=0&portrait=0",
         option2Link:"https://player.vimeo.com/video/935321011?h=9a04ff9c86&autoplay=1&title=0&byline=0&portrait=0",
-        watched: false,
         desc: "Max wakes up from dream"
     },
     { 
@@ -102,7 +103,6 @@ let videosArray = [
         option2: 'kick option',
         option1Link:"https://player.vimeo.com/video/935318444?h=1c05a2036a&autoplay=1&title=0&byline=0&portrait=0",
         option2Link:"https://player.vimeo.com/video/935318444?h=1c05a2036a&autoplay=1&title=0&byline=0&portrait=0",
-        watched: false,
         desc: "Gang chooses to kick Henri out"
     },
     { 
@@ -111,7 +111,6 @@ let videosArray = [
         option2: 'stay option',
         option1Link:"https://player.vimeo.com/video/935318444?h=1c05a2036a&autoplay=1&title=0&byline=0&portrait=0",
         option2Link:"https://player.vimeo.com/video/935318444?h=1c05a2036a&autoplay=1&title=0&byline=0&portrait=0",
-        watched: false,
         desc: "Gang chooses to let Henri stay"
     }
 ];
